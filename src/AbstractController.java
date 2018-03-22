1a7b2abe15524b382431c6abc88301a1b586fad0
Pierre-André Léger <pierrealeger83@gmail.com>
2018-03-22 06:40:31 -0400

    AbstractController javadoc'd

--- src/AbstractController.java
+++ src/AbstractController.java
@@ -1,9 +1,20 @@
 import java.awt.event.ActionListener;
 
+/**
+ * Base class for controller objects.
+ * Classes that inherit should specialize the template with the Interface type
+ * of the view it is associated with.
+ * @param <T> Interface type of the associated view
+ */
 public abstract class AbstractController<T> implements ActionListener {
     protected T view;
     protected IModelController model;
 
+    /**
+     * Constructor.
+     * @param view associated view interface
+     * @param model associated model interface
+     */
     public AbstractController(T view, IModelController model) {
         this.model = model;
         this.view = view;
